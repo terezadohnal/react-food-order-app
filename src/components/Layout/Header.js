8a63ef42acc821e6b75e614c3d2d12b2163de72d
Tereza Dohnalova <tddohnalova@gmail.com>
2022-02-10 14:31:08 +0100

    Add header, Meal List, Cart button

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import HeaderCartBtn from "./HeaderCartBtn";
+
+import mealsImg from "./../../assets/meals.png";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartBtn />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImg} alt="A table full of good food!" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/UI/Input.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import classes from "./Input.module.css";
+
+const Input = (props) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>{props.label}</label>
+      <input {...props.input} />
+    </div>
+  ); // {...props.input} => zkopirujou se vsechny key value pairs
+};
+
+export default Input;
